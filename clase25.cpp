5afb1416f43206973ce71978b81c115a531cda4d
Mafe Morris <mafemorris99@gmail.com>
2019-11-05 17:31:25 -0500

    Hace la matriz la imprime, la retorna, imprime valores deseados.

--- clase25.cpp
+++ clase25.cpp
@@ -4,33 +4,58 @@ using std::cout;
 using std::endl;
 using std::cin;
 
-float **matriz;
-float relleno(float **matriz, int filas, int columnas);
+float** relleno(float **, int, int);
+int imprimeCabezas(float**, int, int);
 
 int main(){
+    float ** matriz;
     int numero;
-    cout<< "Escriba un numero del 3 al 11" << endl;
+
+    cout<< "Escriba un numero del 3 al 11\n";
     cin >> numero;
-    cout<<"2";
-    if(numero<3 || numero>11){
-        cout<<"ingreso el numero incorrecto"<< endl;
-        cout<< "Escriba un numero del 3 al 11" << endl;
+    
+    while(numero<3 || numero>11){
+        cout<<"ingreso el numero incorrecto\n";
+        cout<< "Escriba un numero del 3 al 11\n";
         cin >> numero;
     }
-    
+
     matriz = new float*[numero+2];
+
     relleno(matriz,numero+2,numero);
+
+    imprimeCabezas(matriz,numero+2,numero);
+
+    delete matriz;
+
     return 0;
 }
 
-float relleno(float **matriz, int filas, int columnas){
+float** relleno(float** m, int filas, int columnas){
+    cout << endl;
     for(int i = 0; i<filas; i++){
-        cout<<"1";
+        m[i] = new float [columnas];
         for(int j = 0; j<columnas; j++){
-            matriz[i][j] = i+j;
-            cout<<matriz[i][j]<<endl;
+            m[i][j] = i+j;
+            cout<<m[i][j]<<"\t";
         }
+        cout << endl;
     }
+    cout << endl;
+    return 0;
+}
+
+int imprimeCabezas(float** m,int filas, int columnas){
+    int suma=0;
+    cout<< "La primera fila es: ";
+    for(int i=0;i<filas;i++){
+        cout << *m[i] << " ";
+    }
+    cout << endl;
+    for(int i=0; i<columnas;i++){
+        suma += m[0][i];
+    }
+    cout << "La suma de los primeros elementos de cada columna es: " << suma << endl;
     return 0;
 }
 
